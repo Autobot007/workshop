8eab34fc9d1b0d00da1c0bdeb66be542d4b01260
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-30 09:19:28 +0530

    [SARFARAAZ] WorkShop Task Added

---
+++ TicTacToe/TicTacToeClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class TicTacToeClass
+    {
+        public void UC1Method()
+        {
+            char[] board = new char[10];
+
+
+            int entry = Convert.ToInt32(Console.ReadLine());
+
+            board[entry - 1] = 'X';
+        }
+    }
+}
